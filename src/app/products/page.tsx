3aa7e8c77fc67508bcf19216cffd0e9f32981556
Sabarizi <160276605+Sabarizi@users.noreply.github.com>
2025-01-19 15:21:07 +0500

    add

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -31,7 +31,7 @@ const ProductsPage = async () => {
     <div className="p-10 bg-gray-100 min-h-screen">
       {/* Hero Section */}
       <div className="text-center mb-10">
-        <h1 className="text-4xl font-bold text-gray-900 dark:text-white">
+        <h1 className="text-4xl font-bold text-gray-900">
           Our Products
         </h1>
         <p className="text-gray-600 dark:text-gray-300 mt-2">
